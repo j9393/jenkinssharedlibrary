ac754293d38e3c859463c803cb75624e1b3ea32b
j9393 <58584325+j9393@users.noreply.github.com>
2021-07-07 06:20:03 +0000

    totalvariable

--- src/com/cleverbuilder/Sampletestclass.groovy
+++ src/com/cleverbuilder/Sampletestclass.groovy
@@ -3,9 +3,12 @@ package com.cleverbuilder
 
 
 class Sampletestclass {
+   
+   Integer total;
+	
    def sum(int a,int b) {
-      int c = a+b;
-      println(c);
+      this.total = a+b;
+   
    }  
 	
 }
